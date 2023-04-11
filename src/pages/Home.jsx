3c34260e7f6a3a7eece52faf2581e70369a1538c
Artem <artemtricks@yandex.ru>
2023-04-11 18:35:42 +0700

    add .

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import PizzaBlock from "../components/PizzaBlock";
 import Categories from "../components/Categories";
 import Sort from "../components/Sort";
 
-function Home() {
+function Home({searchValue}) {
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
   const [categoryId, setCategoryId] = React.useState(0);
@@ -20,8 +20,9 @@ function Home() {
     const order = sortType.sortProperty.includes("-") ? "asc" : "desc";
     const sortBy = sortType.sortProperty.replace("-", "");
     const category = categoryId > 0 ? `category=${categoryId}` : "";
+    const search = searchValue ? `&search=${searchValue}` : '';
     fetch(
-      `https://63f36531fe3b595e2ee0f355.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`
+      `https://63f36531fe3b595e2ee0f355.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}${search}`
     )
       .then((res) => {
         return res.json();
@@ -32,7 +33,20 @@ function Home() {
       });
 
     window.scroll(0, 0);
-  }, [categoryId, sortType]);
+  }, [categoryId, sortType, searchValue]);
+
+  const pizzas = items.map((obj) => (
+    <PizzaBlock
+      key={obj.id}
+      title={obj.title}
+      price={obj.price}
+      imageUrl={obj.imageUrl}
+      sizes={obj.sizes}
+      types={obj.types}
+    />
+  ))
+
+  const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />)
 
   return (
     <div className="container">
@@ -46,17 +60,8 @@ function Home() {
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
         {isLoading
-          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-          : items.map((obj) => (
-              <PizzaBlock
-                key={obj.id}
-                title={obj.title}
-                price={obj.price}
-                imageUrl={obj.imageUrl}
-                sizes={obj.sizes}
-                types={obj.types}
-              />
-            ))}
+          ? skeletons
+          : pizzas}
       </div>
     </div>
   );
