78627a61be57828ce9bfa7c5a7f261400040c08d
Artem Kabanov <ar.kabanov@els24.com>
2023-05-28 14:29:10 +0700

    useRef and useCallback on newpage

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import NotFound from "./pages/NotFound";
 import { Routes, Route } from "react-router-dom";
+import NewPage from "./pages/newpage/newPage";
 
 export const SearchContext = React.createContext();
 
@@ -22,6 +23,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/cart" element={<Cart />} />
+            <Route path="/newpage" element={<NewPage />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
